be285f1591e3ee15d9f9428f87406767823c59fa
uben01 <uto.bence@gmail.com>
2020-03-20 14:32:14 +0100

    Naming convection and optimization

--- Final-project/moverClass.js
+++ Final-project/moverClass.js
@@ -1,24 +1,25 @@
 var V7Z3T5 = function () {
     // HELPER CLASSES
     function HeuristicMap() {
-        let finishValue = 100;
-        let sizeX, sizeY, matrix = null;
+        let _finishValue = 100;
+        let _sizeX, _sizeY, _matrix;
 
-        this.initialize = function(c) {
-            sizeX = c.length;
-            sizeY = c[0].length;
-            matrix = [...Array(sizeX)].map(x => Array(sizeY)); // initialize empty arrays of arrays
+        this.initialize = function (c) {
+            _sizeX = c.length;
+            _sizeY = c[0].length;
+            _matrix = [...Array(_sizeX)].map(x => Array(_sizeY)); // initialize empty arrays of arrays
             mapping(c);
+            console.log(_matrix);
         };
 
         let stateTransition = function (state) {
             let newStates = [];
             for (let i = state.x - 1; i <= state.x + 1; i++) {
-                if (i < 0 || i >= sizeX) { // out of x bound
+                if (i < 0 || i >= _sizeX) { // out of x bound
                     continue;
                 }
                 for (let j = state.y - 1; j <= state.y + 1; j++) {
-                    if (j < 0 || j >= sizeY) { // out of y bound
+                    if (j < 0 || j >= _sizeY) { // out of y bound
                         continue;
                     }
                     newStates.push({x: i, y: j, cost: 0});
@@ -29,14 +30,15 @@ var V7Z3T5 = function () {
 
         let sortList = function (list) {
             return list.sort(function (a, b) {
-                if (this[a.x][a.y] == undefined && this[b.x][b.y] == undefined) {
+                if (!this[a.x][a.y] || !this[b.x][b.y]) {
+                    if (!this[a.x][a.y]) {
+                        return -1;
+                    }
+                    if (!this[b.x][b.y]) {
+                        return 1;
+                    }
+
                     return 0;
-                }
-                if (this[a.x][a.y] == undefined) {
-                    return -1;
-                }
-                if (this[b.x][b.y] == undefined) {
-                    return 1;
                 }
                 if (this[a.x][a.y] < this[b.x][b.y]) {
                     return -1;
@@ -45,12 +47,12 @@ var V7Z3T5 = function () {
                     return 1;
                 }
                 return 0;
-            }.bind(matrix));
+            }.bind(_matrix));
         };
 
         let isMemberWithLEValue = function (list, node) {
             for (let i = 0; i < list.length; i++) {
-                if (list[i].x == node.x && list[i].y == node.y) {
+                if (lc.equalPoints(list[i], node)) {
                     if (list[i].cost <= node.cost) {
                         return true;
                     }
@@ -60,8 +62,8 @@ var V7Z3T5 = function () {
         };
 
         let isInMatrixWithLEValue = function (node) {
-            if (matrix[node.x][node.y] != undefined) {
-                if (matrix[node.x][node.y] < node.cost) {
+            if (_matrix[node.x][node.y] != undefined) {
+                if (_matrix[node.x][node.y] < node.cost) {
                     return true;
                 }
             }
@@ -71,9 +73,9 @@ var V7Z3T5 = function () {
         let mapping = function (c) {
             // find finish lines
             let finishLines = [];
-            for (let i = 0; i < sizeX; i++) {
-                for (let j = 0; j < sizeY; j++) {
-                    if (c[i][j] == finishValue) {
+            for (let i = 0; i < _sizeX; i++) {
+                for (let j = 0; j < _sizeY; j++) {
+                    if (c[i][j] == _finishValue) {
                         finishLines.push({x: i, y: j});
                     }
                 }
@@ -81,7 +83,7 @@ var V7Z3T5 = function () {
             for (let i = 0; i < finishLines.length; i++) {
                 let prQueue = [];
                 prQueue.push({x: finishLines[i].x, y: finishLines[i].y, cost: 0});
-                matrix[finishLines[i].x][finishLines[i].y] = 0;
+                _matrix[finishLines[i].x][finishLines[i].y] = 0;
                 while (prQueue.length > 0) {
                     const act = prQueue.shift();
                     let newNodes = stateTransition(act);
@@ -91,10 +93,10 @@ var V7Z3T5 = function () {
                             if (!isMemberWithLEValue(prQueue, newNodes[i]) &&
                                 !isInMatrixWithLEValue(newNodes[i])) {
                                 prQueue.push(newNodes[i]);
-                                matrix[newNodes[i].x][newNodes[i].y] = newNodes[i].cost;
+                                _matrix[newNodes[i].x][newNodes[i].y] = newNodes[i].cost;
                             }
                         } else {
-                            matrix[newNodes[i].x][newNodes[i].y] = act.cost + 1 + 1000; // APPLY PENALTY
+                            _matrix[newNodes[i].x][newNodes[i].y] = act.cost + 1 + 1000; // APPLY PENALTY
                         }
                     }
 
@@ -104,80 +106,90 @@ var V7Z3T5 = function () {
         };
 
         this.distance = function (from, to) {
-            return matrix[from.x][from.y] - matrix[to.x][to.y];
+            return _matrix[from.x][from.y] - _matrix[to.x][to.y];
         };
 
         this.isFinish = function (node) {
-            return matrix[node.getCenter().x][node.getCenter().y] == 0;
+            return _matrix[node.getCenter().x][node.getCenter().y] == 0;
         };
     }
 
     function Node() {
-        let center, distance, stepsTaken, firstNode, velocity;
+        let _center, _distance, _stepsTaken, _firstNode, _velocity;
 
-        this.initialize = function(_center, _prev, _nowDistance) {
-            center = _center;
-            distance = _nowDistance;
-            stepsTaken = 0;
-            firstNode = null;
-            velocity = {};
+        this.initialize = function (center, prev, nowDistance) {
+            _center = center;
+            _distance = nowDistance;
+            _stepsTaken = 0;
+            _firstNode = null;
+            _velocity = {};
 
-            if (_prev) {
-                distance += _prev.getDistance();
-                stepsTaken += _prev.getStepsTaken() +1;
-                firstNode = _prev.getFirstNode();
+            if (prev) {
+                _distance += prev.getDistance();
+                _stepsTaken += prev.getStepsTaken() + 1;
+                _firstNode = prev.getFirstNode();
 
-                velocity = {
-                    x: center.x - _prev.getCenter().x,
-                    y: center.y - _prev.getCenter().y
+                _velocity = {
+                    x: center.x - prev.getCenter().x,
+                    y: center.y - prev.getCenter().y
                 };
             }
         };
 
-        this.h = function() {
-            return distance / stepsTaken;
+        this.h = function () {
+            return _distance / _stepsTaken;
         };
 
-        this.getCenter = function(){
-            return center;
+        this.getCenter = function () {
+            return _center;
         };
-        this.getDistance = function(){
-            return distance;
+        this.getDistance = function () {
+            return _distance;
         };
-        this.getStepsTaken = function(){
-            return stepsTaken;
+        this.getStepsTaken = function () {
+            return _stepsTaken;
         };
-        this.getFirstNode = function(){
-            return firstNode;
+        this.getFirstNode = function () {
+            return _firstNode;
         };
-        this.getVelocity = function(){
-            return velocity;
+        this.getVelocity = function () {
+            return _velocity;
         };
-        this.setVelocity = function(_velocity){
-            velocity = _velocity;
+        this.setVelocity = function (velocity) {
+            _velocity = velocity;
         };
-        this.setFirstNode = function(_firstNode){
-            firstNode = _firstNode;
+        this.setFirstNode = function (firstNode) {
+            _firstNode = firstNode;
         };
     }
 
     // STATE VARIABLES
     let heuristicMap;
     let bound;
-    const initLimit = 10000, moveLimit = 1000;
     let timeLimit;
+    const initLimit = 10000, moveLimit = 1000;
 
     // HELPER FUNCTIONS
-    let inBound = function(node, bound) {
+    let inBound = function (node, bound) {
         return node.getStepsTaken() < bound;
     };
 
-    let resetBound = function(){
+    let resetBound = function () {
         bound = 4;
     };
 
     let sortList = function (list) {
         return list.sort(function (a, b) {
+            if(heuristicMap.isFinish(a) || heuristicMap.isFinish(b)){
+                if(heuristicMap.isFinish(a)){
+                    return -1;
+                }
+                if(heuristicMap.isFinish(b)){
+                    return 1;
+                }
+                return 0;
+            }
+
             if (a.h() > b.h()) {
                 return -1;
             }
@@ -188,7 +200,7 @@ var V7Z3T5 = function () {
         });
     };
 
-    let timeLeft = function(){
+    let timeLeft = function () {
         return timeLimit - Date.now();
     };
 
@@ -202,38 +214,37 @@ var V7Z3T5 = function () {
 
     this.movefunction = function (c, playerdata, selfindex) {
         timeLimit = Date.now() + moveLimit;
-        const self = playerdata[selfindex]; // read the info for the actual player
+        const self = playerdata[selfindex];
 
-        const velocity = {
+        const nowVelocity = {
             x: (self.pos.x - self.oldpos.x),
             y: (self.pos.y - self.oldpos.y)
         };
-        const newCenter = { // that's how the center of the next movement can be computed
+        const nowCenter = {
             x: self.pos.x,
             y: self.pos.y
         };
 
         let validNodes = [];
-
         {
             // create pseudo node
             let node = new Node();
-            node.initialize(newCenter, null, 0);
-            node.setVelocity(velocity);
+            node.initialize(nowCenter, null, 0);
+            node.setVelocity(nowVelocity);
             validNodes.push(node);
         }
         resetBound();
 
         let index = 0;
         let isFinishNodeFound = false;
-        while (validNodes.length > 0) {
+        while (validNodes.length) {
             if (index < validNodes.length) {
                 if (!inBound(validNodes[index], bound) || heuristicMap.isFinish(validNodes[index])) {
                     index++;
                     continue;
                 }
             } else {
-                if(!isFinishNodeFound){
+                if (!isFinishNodeFound) {
                     bound++;
                     index = 0;
                     console.log("BOUND INCREASED TO: " + bound);
@@ -245,48 +256,55 @@ var V7Z3T5 = function () {
             let startingNode = validNodes.splice(index, 1)[0];
             index = 0;
             let distance = null;
-            // we try the possible movements
+
             for (let i = -1; i <= 1; i++) {
                 for (let j = -1; j <= 1; j++) {
-                    const nextMove = { // that's how the center of the next movement can be computed
+                    const nextMove = {
                         x: startingNode.getCenter().x + (startingNode.getVelocity().x + i),
                         y: startingNode.getCenter().y + (startingNode.getVelocity().y + j)
                     };
-                    //let nextMove = {x: startingNode.coordinates.x + i, y: startingNode.coordinates.y + j};
-                    // if the movement is valid (the whole line has to be valid)
-                    if (lc.validLine(startingNode.getCenter(), nextMove) && (startingNode.getFirstNode() || (lc.playerAt(nextMove) < 0 || lc.playerAt(nextMove) == selfindex))) {
+
+                    if(lc.equalPoints(self.oldpos, self.pos) && lc.equalPoints(self.pos, nextMove))
+                        continue;
+
+                    if (lc.validLine(startingNode.getCenter(), nextMove) && (startingNode.getFirstNode() || (lc.playerAt(nextMove) < 0 || lc.playerAt(nextMove) === selfindex))) {
                         distance = heuristicMap.distance(startingNode.getCenter(), nextMove);
 
-                        let node = new Node(); node.initialize(nextMove, startingNode, distance);
+                        let node = new Node();
+                        node.initialize(nextMove, startingNode, distance);
                         if (!node.getFirstNode()) {
                             node.setFirstNode(node);
                         }
-                        if (node.getStepsTaken() != 0 || distance != 0 || (Math.abs(node.getVelocity().x) > 0 && Math.abs(node.getVelocity().y) > 0)) {
-                            validNodes.push(node);
-                            if (heuristicMap.isFinish(node)) {
-                                isFinishNodeFound = true;
-                                if (bound > node.getStepsTaken()) {
-                                    bound = node.getStepsTaken();
-                                }
-                            }
+                        validNodes.push(node);
+                        if (heuristicMap.isFinish(node)) {
+                            isFinishNodeFound = true;
+                            bound = node.getStepsTaken();
+                            validNodes = validNodes.filter(
+                                value => (
+                                    value.getStepsTaken() < bound ||
+                                    (value.getStepsTaken() === bound && heuristicMap.isFinish(value))
+                                ));
                         }
                     }
                 }
             }
-            if (isFinishNodeFound) {
-                validNodes = validNodes.filter(value => (value.getStepsTaken() < bound || (value.getStepsTaken() == bound && heuristicMap.isFinish(value))));
-            }
-            sortList(validNodes, heuristicMap);
-            if(timeLeft() < 51){
+            sortList(validNodes);
+
+            if (timeLeft() < 51 || validNodes.length === 1) {
                 break;
+            }
+            if(validNodes.length > 700){
+                validNodes = validNodes.splice(0, 700);
             }
         }
 
-        let move = {x: 0, y: 0};
+        let move;
         if (validNodes.length) {
             move = validNodes.shift().getFirstNode().getCenter();
-            move.x -= newCenter.x + velocity.x;
-            move.y -= newCenter.y + velocity.y;
+            move.x -= nowCenter.x + nowVelocity.x;
+            move.y -= nowCenter.y + nowVelocity.y;
+        } else{
+            move = {x: 0, y: 0};
         }
         console.log("MOVE TIME LIMIT: ", timeLeft());
         return move;
